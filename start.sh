570d3e128291ac6fa41c978a6373e8dbd4811e35
Sigurre <sigurre@icloud.com>
2014-05-06 19:45:39 -0600

    Fixed(?) server script

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd "$BINDIR"
 
 while true
 do
-    java -server -Xms1024M -Xmx1024M -jar craftbukkit.jar
+    java -server -Xms1024M -Xmx1024M -jar server.jar
 
     echo -e 'If you want to completely stop the server process now, press ctrl-C before the\ntime is up!'
     echo "Rebooting in:"
