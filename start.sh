f9b160599ab7fb7d74b68b30d678e1bf99a9d556
Sigurre <sigurre@icloud.com>
2014-05-06 19:40:49 -0600

    Mod and Plugin update
    Removed AutoRestart in order to regenerate appropriate files,
    
    Added Computer Craft 1.6.3.
    
    Edited the start.sh file to loop in order to use Autorestart.

--- start.sh
+++ start.sh
@@ -1,2 +1,18 @@
-java -Xms2048m -Xmx2048m -jar server.jar nogui
-PAUSE
+#!/bin/bash
+
+BINDIR=$(dirname "$(readlink -fn "$0")")
+cd "$BINDIR"
+
+while true
+do
+    java -server -Xms1024M -Xmx1024M -jar craftbukkit.jar
+
+    echo -e 'If you want to completely stop the server process now, press ctrl-C before the\ntime is up!'
+    echo "Rebooting in:"
+    for i in {5..1}
+    do
+        echo "$i..."
+        sleep 1
+    done
+    echo 'Restarting now!'
+done
