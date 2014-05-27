f1d18066b6a6f1b9387a800fcc0815f351fbcc73
Sigurre <sigurre@icloud.com>
2014-05-26 20:46:18 -0600

    ThaumicTinkerer/Start script
    Added Thaumic Tinkerer to modpack and server. Also altered startup
    script

--- start.sh
+++ start.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
-			BINDIR=$(dirname "$(readlink -fn "$0")")
-			cd "\$BINDIR"
-			while true
-			do
-				java -Xmx1024M -Xms1024M -jar server.jar
-				echo "If you want to completely stop the server process now, press Ctrl+C before the time is up!"
-				echo "Rebooting in:"
-				for i in 5 4 3 2 1
-				do
-					echo "$i..."
-					sleep 1
-				done
-				echo "Rebooting now!"
-			done
+#!/bin/bash
+
+BINDIR=$(dirname "$(readlink -fn "$0")")
+cd "$BINDIR"
+
+while true
+do
+    java -server -Xms1024M -Xmx1024M -jar craftbukkit.jar
+
+    echo -e 'If you want to completely stop the server process now, press ctrl-C before the\ntime is up!'
+    echo "Rebooting in:"
+    for i in {5..1}
+    do
+        echo "$i..."
+        sleep 1
+    done
+    echo 'Restarting now!'
+done
